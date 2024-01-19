29f38bb4914214981bea85f8848c188d80d3ea56
s2hmuel <s2hmuel@gmail.com>
2024-01-19 18:22:39 +0900

    Init: 초기설정

---
+++ src/main/java/com/ll/tourdemonde/TourDeMondeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.tourdemonde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourDeMondeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourDeMondeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/tourdemonde/TourDeMondeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.tourdemonde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourDeMondeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
