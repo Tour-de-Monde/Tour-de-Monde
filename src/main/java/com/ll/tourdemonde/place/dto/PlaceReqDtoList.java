4111252ac5e1941e2e929e3c62acbb0e5b095e94
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-22 13:46:46 +0900

    refactor : 장소가 이미 존재 하는 경우, 장소를 저장 하지 않음 - 장소 1개 등록 엔드포인트 필요없어서 제거 - application.yml 수정, application-test.yml 추가

--- src/main/java/com/ll/tourdemonde/place/dto/PlaceReqDtoList.java
+++ src/main/java/com/ll/tourdemonde/place/dto/PlaceReqDtoList.java
@@ -5,6 +5,6 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class PlaceReqDtoList {
+public class PlaceReqDtoList { // 여러 개의 장소 리스트
     private List<PlaceReqDto> placeReqDtoList;
 }
