d85d7b10370a9b4137cfc587fcf6242a17868e2c
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-23 17:50:04 +0900

    test : 장소 수정 POST /place/modify

--- src/test/java/com/ll/tourdemonde/place/controller/PlaceControllerTest.java
+++ src/test/java/com/ll/tourdemonde/place/controller/PlaceControllerTest.java
@@ -105,7 +105,41 @@ class PlaceControllerTest {
     }
 
     // 장소 수정 POST /place/modify
+    @DisplayName("장소를 수정한다.")
+    @Test
+    void t3() throws Exception {
+        // GIVEN
+        PlaceReqDto placeReqDto = new PlaceReqDto("아름식당", "대구 달서구", "24.5, 34");
+        List<PlaceReqDto> placeReqDtos = new ArrayList<>();
+        placeReqDtos.add(placeReqDto);
+        PlaceReqDtoList placeReqDtoList = new PlaceReqDtoList(placeReqDtos);
+        placeService.save(placeReqDtoList);
 
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/place/1/modify")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("name", "예쁜식당")
+                        .param("address", "대구 수성구")
+                        .param("coordinates", "24.5, 34")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions.andExpect(status().isOk())
+                .andExpect(view().name("domain/place/test"))
+                .andExpect(handler().handlerType(PlaceController.class))
+                .andExpect(handler().methodName("modifyPlace"));
+
+        // 검증
+        /*PlaceReqDto placeReqDtoTest = new PlaceReqDto("예쁜식당", "대구 수성구", "24.5, 34");
+        Place place = placeService.findPlace(placeReqDtoTest).getData();*/
+        Place place = placeService.findByPlace(1L).getData();
+
+        assertThat(place.getName()).isEqualTo("예쁜식당");
+        assertThat(place.getAddress()).isEqualTo("대구 수성구");
+        assertThat(place.getCoordinates()).isEqualTo("24.5, 34");
+    }
 
     // 장소 삭제 POST /place/delete
     @DisplayName("장소를 삭제한다.")
