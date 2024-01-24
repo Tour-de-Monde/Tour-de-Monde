5015c2a1ac28a8ced1c27526e739efe81ce66cb8
dongha922 <kdh4575@naver.com>
2024-01-24 17:29:51 +0900

    지도생성

--- src/main/java/com/ll/tourdemonde/AddressController.java
+++ src/main/java/com/ll/tourdemonde/AddressController.java
@@ -1,13 +1,13 @@
-package com.ll.tourdemonde;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-@Controller
-public class AddressController {
-    @GetMapping("/address")
-    @ResponseBody
-    public String address(){
-        System.out.println("카카오 테스트");
-        return  "address";
-    }
-
-}
+//package com.ll.tourdemonde;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//@Controller
+//public class AddressController {
+//    @GetMapping("/address")
+//    @ResponseBody
+//    public String address(){
+//        System.out.println("카카오 테스트");
+//        return  "address";
+//    }
+//
+//}
