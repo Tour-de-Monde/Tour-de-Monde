b065925bd6df147a44b4217be664716aa3eae2d6
geniushee <129508219+geniushee@users.noreply.github.com>
2024-01-30 14:53:50 +0900

    Enhancement/6 - 테스트로 인해 발생하는 에러 처리 완료 (#14)
    * hotfix : initdata 미성으로 인한 테스트 안되던 문제 해결
    
    ---------
    
    Co-authored-by: Son-Gyeongi <thsruddl77@gmail.com>

--- src/main/java/com/ll/tourdemonde/reservation/devinit/ReservationInit.java
+++ src/main/java/com/ll/tourdemonde/reservation/devinit/ReservationInit.java
@@ -24,12 +24,7 @@ public class ReservationInit implements ApplicationRunner {
     @Override
     @Transactional
     public void run(ApplicationArguments args) {
-        IntStream.range(1, 4).mapToObj(i -> {
-            return new PlaceDto("장소" + i, "23.1, 35." + i);}
-                )
-                .map(placeDto -> {
-                    placeService.save(placeDto);
-                    return null;
-                });
+        IntStream.range(1, 4).mapToObj(i -> new PlaceDto("장소"+i, "33.1, 37.1" + i))
+                .forEach(placeService::save);
     }
 }
