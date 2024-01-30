852c96aef5c57fcfd60f8692130f09ab8b8cda5d
dongha922 <kdh4575@naver.com>
2024-01-30 13:43:49 +0900

    ãhomecontrolleãr 충돌

--- src/main/java/com/ll/tourdemonde/home/controller/HomeController.java
+++ src/main/java/com/ll/tourdemonde/home/controller/HomeController.java
@@ -4,6 +4,7 @@ package com.ll.tourdemonde.home.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 
 public class HomeController {
@@ -13,4 +14,16 @@ public class HomeController {
         return "redirect:/post/list";
 
     }
+
+    @GetMapping("/map")
+//    @ResponseBody
+    String showMap(){
+        return "map";
+    }
+    @GetMapping("/search")
+//    @ResponseBody
+    String showSearch(){
+        return "search";
+    }
+
 }
