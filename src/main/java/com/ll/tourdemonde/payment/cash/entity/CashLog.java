94265a443dc4df11b150d91e9b7caf1e225bd4a5
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-31 06:32:12 +0900

    feat : CashLog 추가 - 결제 시 캐시 사용 로그 - entity, repository, service 틀 잡아놓음

---
+++ src/main/java/com/ll/tourdemonde/payment/cash/entity/CashLog.java
@@ -0,0 +1,35 @@
+package com.ll.tourdemonde.payment.cash.entity;
+
+import com.ll.tourdemonde.member.entity.Member;
+import com.ll.tourdemonde.post.entity.BaseTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Setter
+@Getter
+@ToString(callSuper = true)
+public class CashLog extends BaseTime {
+    @Enumerated(EnumType.STRING)
+    private EventType eventType;
+    private String relTypeCode;
+    private Long relId;
+    @ManyToOne
+    private Member member;
+    private long price;
+
+    public enum EventType {
+        충전__무통장입금,
+        충전__토스페이먼츠,
+        출금__통장입금,
+        사용__토스페이먼츠_주문결제,
+        사용__예치금_주문결제,
+        환불__예치금_주문결제,
+    }
+}
