2817c819a776fbb2a4415a66f4b6f9c400f268ac
kds98 <skyudae@naver.com>
2024-01-31 10:52:55 +0900

    refactor : 코드 리팩토링

--- src/main/java/com/ll/tourdemonde/global/security/SecurityConfig.java
+++ src/main/java/com/ll/tourdemonde/global/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.net.URLEncoder;
@@ -23,18 +24,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/gen/**")
-                                .permitAll()
-                                .requestMatchers("/resource/**")
-                                .permitAll()
-                                .requestMatchers("/h2-console/**")
-                                .permitAll()
-                                .requestMatchers("/adm/**")
-                                .hasRole("ADMIN")
-                                .anyRequest()
-                                .permitAll()
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/gen/**")
+                        .permitAll()
+                        .requestMatchers("/resource/**")
+                        .permitAll()
+                        .requestMatchers("/h2-console/**")
+                        .permitAll()
+                        .requestMatchers("/adm/**")
+                        .hasRole("ADMIN")
                 )
                 .headers(
                         headers ->
@@ -61,6 +59,20 @@ public class SecurityConfig {
                                 logout
                                         .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
                 )
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()) // 로그인 하지 않은 경우에도 모든 페이지 접근 허용
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))) // h2-console에 한해 csrf 검증 해제
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/member/signin")
+                        .defaultSuccessUrl("/")) // 스프링 시큐리티에 로그인 URL 등록
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)) // 스프링 시큐리티에 로그아웃 URL 등록
                 .oauth2Login(
                         oauth2Login -> oauth2Login
                                 .loginPage("/member/login")
