87ccc81d76c4ee84a230a0fd364662a941cdfec3
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-31 20:20:10 +0900

    feat : addCash() 추가

--- src/main/java/com/ll/tourdemonde/payment/cash/service/CashService.java
+++ src/main/java/com/ll/tourdemonde/payment/cash/service/CashService.java
@@ -1,6 +1,9 @@
 package com.ll.tourdemonde.payment.cash.service;
 
+import com.ll.tourdemonde.member.entity.Member;
+import com.ll.tourdemonde.payment.cash.entity.CashLog;
 import com.ll.tourdemonde.payment.cash.repository.CashLogRepository;
+import com.ll.tourdemonde.reservation.entity.Reservation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class CashService {
     private final CashLogRepository cashLogRepository;
+
+    // 캐시 로그 저장
+    @Transactional
+    public CashLog addCash(Member member, long price, CashLog.EventType eventType, Reservation reservation) {
+        CashLog cashLog = CashLog.builder()
+                .member(member)
+                .price(price)
+                .relTypeCode(reservation.getType().name())
+                .eventType(eventType)
+                .build();
+
+        cashLogRepository.save(cashLog);
+
+        return cashLog;
+    }
 }
