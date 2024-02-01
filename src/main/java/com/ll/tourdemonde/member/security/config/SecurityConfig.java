070e479f1b37e1e723bd152ceee74d47822efbf7
Aiden Lee <yw010804@gmail.com>
2024-02-01 17:07:14 +0900

    Enhancement/5 (#23) - 회원가입 오류 관련 어노테이션 추가

--- src/main/java/com/ll/tourdemonde/member/security/config/SecurityConfig.java
+++ src/main/java/com/ll/tourdemonde/member/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +16,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
