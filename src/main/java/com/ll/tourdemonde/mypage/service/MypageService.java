eed88c397684688639962531ac904f014f0e7d18
Aidennn-Lee <yw010804@gmail.com>
2024-02-16 00:46:39 +0900

    feat : 예약 내역 페이징 처리

--- src/main/java/com/ll/tourdemonde/mypage/service/MypageService.java
+++ src/main/java/com/ll/tourdemonde/mypage/service/MypageService.java
@@ -6,8 +6,6 @@ import com.ll.tourdemonde.payment.order.entity.Order;
 import com.ll.tourdemonde.payment.order.repository.OrderRepository;
 import com.ll.tourdemonde.post.entity.Post;
 import com.ll.tourdemonde.post.repository.PostRepository;
-import com.ll.tourdemonde.reservation.entity.Reservation;
-import com.ll.tourdemonde.reservation.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,7 +16,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -39,21 +36,19 @@ public class MypageService {
 
     //현재 로그인한 사용자가 좋아요 한 글 리스트 반환
     public Page<Post> votePostList(String username, int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
         Optional<Member> member = memberRepository.findByUsername(username);
-        Pageable pageable = PageRequest.of(page, 10);
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.postRepository.findAllByVoterContains(member.get(), pageable);
     }
 
     //현재 로그인한 사용자의 예약 리스트 반환
-    public List<Order> myOrderList(String username) {
-        List<Order> orderList = orderRepository.findAll();
+    public Page<Order> myOrderList(String username, int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("payDate"));
         Optional<Member> member = memberRepository.findByUsername(username);
-        Long id = member.get().getId();
-
-        List<Order> myOrderList = orderList.stream()
-                .filter(order -> order.getBuyer().getId().equals(id))
-                .collect(Collectors.toList());
-
-        return myOrderList;
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.orderRepository.findAllByBuyer(member.get(), pageable);
     }
 }
